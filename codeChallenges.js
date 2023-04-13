378ec939547b1cd166113429cfeb83c8cec8fea1
“Bethany <Bethanygellington@gmail.com>
2023-04-13 16:09:08 +0800

    write challenge one

---
+++ codeChallenges.js
@@ -0,0 +1,8 @@
+// Code Challenge number 1
+// Write a function that takes the base and height of a triangle and return its area.
+function calcAreaOfTriangle(base, height) {
+    return base * height / 2;
+}
+//test the function
+console.log(calcAreaOfTriangle(30, 15))
+
