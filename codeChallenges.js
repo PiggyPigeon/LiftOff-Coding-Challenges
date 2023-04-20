51846b8093d81a909667859ec08b095f0511921e
“Bethany <Bethanygellington@gmail.com>
2023-04-20 11:23:11 +0800

    code week 2

--- codeChallenges.js
+++ codeChallenges.js
@@ -1,4 +1,4 @@
-// Code Challenge number 1
+// Code Challenge week 1
 // Write a function that takes the base and height of a triangle and return its area.
 function calcAreaOfTriangle(base, height) {
     return base * height / 2;
@@ -6,3 +6,14 @@ function calcAreaOfTriangle(base, height) {
 //test the function
 console.log(calcAreaOfTriangle(30, 15))
 
+//Code Challenge week 2
+function minMax(numberArray) {
+    let minValue = Math.min(...numberArray);
+    let maxValue = Math.max(...numberArray);
+    return [minValue, maxValue];
+}
+
+console.log(minMax([10, 5, 8, 3, 12, 7]));
+
+
+//Code Challenge week 3
