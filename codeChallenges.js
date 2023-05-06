d3dcd4f2e4065b78cffc27e00e34c330e427d51d
“Bethany <Bethanygellington@gmail.com>
2023-05-06 13:02:33 +0800

    write challenge four

--- codeChallenges.js
+++ codeChallenges.js
@@ -26,3 +26,34 @@ function FirstReverse(str) {
 
 // keep this function call here
 console.log(FirstReverse(readline()));
+
+
+//Code Challenge week 4
+
+let cat = "i%^& love12#$ catssss^&812"
+let test = "test that if words are of equal leng@# @#th the first one gets retu^&*& rned$%";
+function LongestWord(sen) {
+//remove non-abc or 123 characters, replace them with a space
+    sen = sen.replace(/[^0-9a-z]/gi, ' ');
+
+//split string into an array, delimited by a space.
+    words = sen.split(" ");
+
+//initialize the longest variable
+    let longest = '';
+//loop through words array,
+//check current values length and compare to 'longest' variable length.
+//if current value larger, replace longest with current value.
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longest.length) {
+            longest = words[i];
+        }
+    }
+    return longest;
+}
+
+console.log(LongestWord(cat));
+console.log(LongestWord(test));
+
+// CodeByte keep this function call here
+console.log(LongestWord(readline()));
