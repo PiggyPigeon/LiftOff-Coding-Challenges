c533ea05b3a219c2f442340704f1620572c86020
“Bethany <Bethanygellington@gmail.com>
2023-05-16 16:41:21 +0800

    write challenge five

--- codeChallenges.js
+++ codeChallenges.js
@@ -57,3 +57,26 @@ console.log(LongestWord(test));
 
 // CodeByte keep this function call here
 console.log(LongestWord(readline()));
+
+
+
+//Code Challenge 5
+// Create a function that takes a number as an argument.
+// Add up all the numbers from 1 to the number you passed to the function.
+// For example, if the input is 4 then your function should
+// return 10 because 1 + 2 + 3 + 4 = 10.
+
+function addUp(x) {
+    //initialize sum
+    sum = 0;
+    // loop through integer
+    for (let i = 0; i < x; i++) {
+        //accumulate
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(addUp(4));
+console.log(addUp(13));
+console.log(addUp(600));
