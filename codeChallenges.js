cccf02b296bfe3636a1d64fc37363eb12b2bd9cb
“Bethany <Bethanygellington@gmail.com>
2023-05-28 20:36:30 +0800

    write challenge six

--- codeChallenges.js
+++ codeChallenges.js
@@ -80,3 +80,22 @@ function addUp(x) {
 console.log(addUp(4));
 console.log(addUp(13));
 console.log(addUp(600));
+
+
+// Code Challenge Six
+// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+//
+//     The wanted word is in lowercase.
+//     The crowd of letters is all in uppercase.
+//     Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+function detectWord(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i].toLowerCase()) {
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+console.log(detectWord('UGHcGFHaHJVBJHGtGJHGJ'))
